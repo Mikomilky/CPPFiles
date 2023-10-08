314827a25d34f175ed07dda2eb8e759a7e333bad
Mikomilky <3082619531@qq.com>
2023-10-08 11:30:25 +0800

    1008

---
+++ LeapYear.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int year;
+	bool isLeapYear;
+
+	cout << "输入年份:";
+	cin >> year;
+		isLeapYear = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+
+	if (isLeapYear)
+		cout << year << "是闰年" << endl;
+	else
+		cout << year << "不是闰年" << endl;
+	return 0;
+}
+
+
+
